7de7e67e84285269403433fedac4313f56e4b92d
Frank Zubko <frank.zubko@gmail.com>
2025-03-14 08:23:32 -0400

    added submit event

--- js/mv/mv-event.js
+++ js/mv/mv-event.js
@@ -6,22 +6,24 @@ export function mvEvent(root, $scope) {
 	// https://developer.mozilla.org/en-US/docs/Web/API/Element#events
 	let eventMap = new Map([
 		//['mv-load', 'load'],
-		['mv-click', 'click']
+		['mv-click', 'click'],
+		['mv-submit', 'submit']
 	]);
 	for (const [attribute, eventName] of eventMap) {
 		const label = $scope.$label + `[${attribute}]`;
 		for (let node of root.querySelectorAll(`:scope [${attribute}]`)){
 			Logger.eventCreate && console.debug('%c' + label, Logger.css.create($scope.$depth));
-			const event = () => {
+			const listener = (event) => {
 				try {
 					Logger.eventInvoke && console.debug('%c' + label, Logger.css.invoke($scope.$depth));
+					event.preventDefault();
 					evaluate.call(node, $scope, node.getAttribute(attribute));
 				} catch (e) {
 					console.warn('%c'+attribute, 'color: darkorange;', '\n', e);
 				}
 			}
-			node.addEventListener(eventName, event);
-			$scope.whenUnload.then(() => removeEventListener(eventName, event))
+			node.addEventListener(eventName, listener);
+			$scope.whenUnload.then(() => removeEventListener(eventName, listener))
 		}
 	}
 }
